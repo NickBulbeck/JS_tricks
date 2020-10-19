71505d9ead56af9d1659cb625a1a5a88c155769e
NickBulbeck <bulbecknick@gmail.com>
2020-10-19 20:10:49 +0100

    Added the <map> element

--- tricks.js
+++ tricks.js
@@ -1,2 +1,14 @@
+//************************************************
 // nearest ancestor
 const usefulElement = event.target.closest('.foadyb') // identifies the nearest ancestor matching the selector
+// The useful thing with this is that you can attach an event-listener to a big parent element, then trigger it
+// on whatever selector it happens within. For instance:
+userfulElement.display = userIndex === 0 ? "none" : "block"
+
+//**************************************************
+// map element
+<p>
+  <img src="images/foadby.jpg" usemap="#workmap">
+  <map name="workmap">
+    <area shape="rect" coords="TLacross,TLdown,BRacross,BRdown" alt="description" href="somewhere else">
+  </map>
