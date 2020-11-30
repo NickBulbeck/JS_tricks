7db2d23201ff059b21c165d2de8c00f0d2a88943
NickBulbeck <bulbecknick@gmail.com>
2020-11-30 10:38:49 +0000

    Corrected typo

--- tricks.js
+++ tricks.js
@@ -3,7 +3,7 @@
 const usefulElement = event.target.closest('.foadyb') // identifies the nearest ancestor matching the selector
 // The useful thing with this is that you can attach an event-listener to a big parent element, then trigger it
 // on whatever selector it happens within. For instance:
-userfulElement.display = userIndex === 0 ? "none" : "block"
+usefulElement.display = userIndex === 0 ? "none" : "block"
 
 //**************************************************
 /* map element
